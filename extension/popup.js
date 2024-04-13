a5e212ffda9989762729d485777cfc31afb0a24c
Raymann Singh <raymann.singh@gmail.com>
2024-04-13 14:28:38 -0500

    opup

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,9 @@
-// popup.js
-chrome.storage.local.get(['translatedText'], (result) => {
-    const translatedResultDiv = document.getElementById('translatedResult');
-    translatedResultDiv.textContent = result.translatedText || 'Translation not available';
+document.addEventListener('DOMContentLoaded', () => {
+  const translatedResultDiv = document.getElementById('translatedResult');
+
+  // Retrieve translated text from local storage
+  chrome.storage.local.get(['translatedText'], (result) => {
+      const translatedText = result.translatedText || 'Translation not available';
+      translatedResultDiv.textContent = translatedText;
   });
-  
+});
