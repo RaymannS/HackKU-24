ce8243c0ed9772342a612a5afffae1fa655877f8
Raymann Singh <raymann.singh@gmail.com>
2024-04-13 15:48:40 -0500

    asd

--- extension/background.js
+++ extension/background.js
@@ -1,36 +1,9 @@
 // background.js
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "translateText") {
-      const textToTranslate = message.text;
-      console.log("Text to translate:", textToTranslate);
-      const apiKey = "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";
-  
-      fetch(`https://api.openai.com/v1/engines/text-davinci-003/completions`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${apiKey}`
-        },
-        body: JSON.stringify({
-          prompt: textToTranslate,
-          max_tokens: 1
-        })
-      })
-      .then(response => response.json())
-      .then(data => {
-        const translatedText = data.choices[0].text.trim();
-        chrome.storage.local.set({ translatedText }); // Store translated text in local storage
-      });
-    }
-
-});
-
 chrome.contextMenus.create({
     "id": "translateSlang",
-    "title": "You selected \"%s\"",
+    "title": "Translate \"%s\"",
     "contexts": ["selection"],
 });
-
 chrome.contextMenus.onClicked.addListener(function(clickData) {
   if (clickData.menuItemId === "translateSlang" && clickData.selectionText) {
     // Save the selected text to be accessed by popup.js
