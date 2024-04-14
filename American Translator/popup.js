08d287da27d3c3f9526af044d15ed7053655e9d6
Raymann Singh <raymann.singh@gmail.com>
2024-04-13 22:03:16 -0500

    Final

--- American Translator/popup.js
+++ American Translator/popup.js
@@ -1,12 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
   const translatedResultDiv = document.getElementById('translatedResult');
-
-  // Retrieve translated text from local storage
   console.log("popup.js ran");
-  /*
-  chrome.storage.local.get(['translatedText'], (result) => {
-      const translatedText = result.translatedText || 'Yiipe Translation not available';
-      translatedResultDiv.textContent = translatedText;
-  });
-  */
 });
