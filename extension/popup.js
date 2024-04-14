ffaa33fa7d9287f3907969cb1903991b63f8c29b
Andrew Evans <aje312@gmail.com>
2024-04-13 21:24:14 -0500

    FINAL

--- extension/popup.js
+++ extension/popup.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Retrieve translated text from local storage
   console.log("popup.js ran");
+  /*
   chrome.storage.local.get(['translatedText'], (result) => {
       const translatedText = result.translatedText || 'Yiipe Translation not available';
       translatedResultDiv.textContent = translatedText;
   });
+  */
 });
