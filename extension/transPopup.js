88e5ceacc3dcc463b130d32e21759184efe27798
Raymann Singh <raymann.singh@gmail.com>
2024-04-13 21:11:31 -0500

    Commit

--- extension/transPopup.js
+++ extension/transPopup.js
@@ -1,17 +1,22 @@
+// transPopup.js
+// When HTML is done loading do this
 document.addEventListener('DOMContentLoaded', () => {
+    // Get 'selectedText' from local storage
     chrome.storage.local.get(['selectedText'], async (result) => {
         const selectedText = result.selectedText || 'No text selected';
+        // Set the popupContent data to the highlighted text in translation.html
         const popupContent = document.getElementById('popupContent');
         popupContent.textContent = selectedText;
+        // POST to localhost server to run puppeteer on selectedText
         try {
-            const response = await fetch('http://192.168.56.1:3000/translate', {
+            const response = await fetch('http://localhost:3000/translate', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({ selectedText })
             });
-
+            // If response then yippee output translatedText
             if (response.ok) {
                 const data = await response.json();
                 const translatedText = data.translatedText;
@@ -21,6 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
             } else {
                 console.error('Translation request failed');
             }
+            // ew yucky error blewph
         } catch (error) {
             console.error('Error:', error);
             transContent.textContent = error;
